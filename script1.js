8da46c177aaec4ea29f3762dacc5dd7d549a76cc
AbderrahimAgra <abder.agra@gmail.com>
2025-10-28 11:39:37 +0100

    add event handler summ()

--- script1.js
+++ script1.js
@@ -1,4 +1,9 @@
-var a =document.getElementById('a').value
-var b =document.getElementById('b').value
-var resault =document.getElementById('resault').innerHTML
-resault = a + b
+
+
+
+function summ(){
+    var b = Number(document.getElementById('b').value)
+    var a = Number(document.getElementById('a').value)
+
+    return document.getElementById("resault").innerHTML = a + b 
+}
